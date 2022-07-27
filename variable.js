bbc1f85d999f8f8a7d108a005234520b1c778e58
Tanjimhasanrobiul <dvillage.pavelab@gmail.com>
2022-07-27 20:25:32 +0600

    string and boolean add

---
+++ variable.js
@@ -0,0 +1,15 @@
+// numberic
+var cok = 20;
+var pepsi = 18;
+var sevenUp = 16;
+
+//  String
+var myName = 'My Name Is Tanjim Hasaan';
+var locotion = 'mirpur';
+var zela = 'Dhaka ';
+
+// Boolean
+var examPass = true;
+var goingSajek = false;
+
+console.log(zela)
