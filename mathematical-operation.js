36547bf972c897a9b49bea3abaa52fdb1e010df1
Tanjimhasanrobiul <dvillage.pavelab@gmail.com>
2022-08-01 18:43:26 +0600

    math calculate solution

---
+++ mathematical-operation.js
@@ -0,0 +1,18 @@
+var eggPrice = 9;
+var orangePrice = 43;
+var applePrice = 220;
+var oil1Kg = 190;
+var oil500Gm = 100;
+var eggQnt = 7;
+
+
+var eggTotalPrice = eggPrice * eggQnt;
+var oilDifferencePrice = oil1Kg - oil500Gm;
+var appleAndOrangePriceDifference = applePrice / orangePrice;
+var totalPrice = eggPrice + orangePrice + applePrice + oil1Kg + oil500Gm;
+
+
+console.log(eggTotalPrice)
+console.log(oilDifferencePrice)
+console.log(appleAndOrangePriceDifference)
+console.log(totalPrice)
